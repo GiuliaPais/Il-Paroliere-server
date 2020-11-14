0d63ec61ae5153bf05025b92c8570baed7fa8a64
AlessandroLerro <alessandrolerro1e@gmail.com>
2020-11-14 14:40:29 +0100

    [FIX] StatManager

--- server.database/src/main/java/uninsubria/server/database/StatManager.java
+++ server.database/src/main/java/uninsubria/server/database/StatManager.java
@@ -8,7 +8,9 @@ public class StatManager {
 
 	private ArrayList<Integer> Game;
 	private ArrayList<String> letters;
-	private ArrayList<Integer> occurences ;
+	private ArrayList<Integer> occurences;
+	private ArrayList<Integer> nMatch;
+	private ArrayList<double> avg;
 	
 	
 	/**
@@ -19,14 +21,14 @@ public class StatManager {
 	 * 
 	 * @return ArrayList<>
 	 */
-	public ArrayList<Integer> avg_grid_occourrencies(ArrayList<Integer> game, String[] grid) {
-		
+	public ArrayList<double> avg_grid_occourrencies(ArrayList<Integer> game, String[] grid, ArrayList<Integer> nMatch) {
+		int index;
 		for(int g: Game) {
 			for(int i=0; i<grid.length; i++) {
 				if(letters.isEmpty() || !letters.contains(grid[i]) || !game.contains(g)) {
 					
 					letters.add(grid[i]);
-					int index = letters.indexOf(grid[i]);
+					index = letters.indexOf(grid[i]);
 					occurences.set(index,occurences.get(index)+1);
 					
 				}else if(game.contains(g)){
@@ -35,13 +37,19 @@ public class StatManager {
 					
 				}else {
 					
-					int index = letters.indexOf(grid[i]);
+					index = letters.indexOf(grid[i]);
 					occurences.set(index,occurences.get(index)+1);
 					
 				}
 			}
+			for(int j=0; j<letters.size(); j++) {
+				
+				avg.set(j, occurences.get(j) / nMatch.get(j));
+				
+			}
+			
 		}		
-		return occurences;
+		return avg;
 	}
 	
 }
