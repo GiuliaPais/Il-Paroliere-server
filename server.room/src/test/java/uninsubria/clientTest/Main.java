8ea7c66aaadf8c9e44df9367e214c744e5c38689
AlessandroLerro <alessandrolerro1e@gmail.com>
2020-11-25 16:30:56 +0100

    prova

--- server.room/src/test/java/uninsubria/clientTest/Main.java
+++ server.room/src/test/java/uninsubria/clientTest/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         int[] ip = {151, 48, 158, 26};
-        String name = "Dave";
+        String name = "Ale";
 
-        Client client = new Client(name);
+        Client client = new Client(name, ip);
         client.start();
     }
 }
